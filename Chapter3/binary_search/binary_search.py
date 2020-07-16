b5d296e851390123d0bf4aaaa2cc8d94389b3485
Theo Dao <trungdao912@gmail.com>
2020-07-16 15:52:11 +0700

    first and last index binary search

--- Chapter3/binary_search/binary_search.py
+++ Chapter3/binary_search/binary_search.py
@@ -34,4 +34,25 @@ def recursion_binary_search(array, target, current_index = 0):
 print(recursion_binary_search([1,2,3,4,5,6], 2))
 
 def find_first_and_last_index(array, target):
-  return target
+  initial_index = binary_search(array, target)
+    
+  if initial_index == -1:
+    return [-1, -1]
+    
+  lower_index = initial_index
+  # lower search
+  while lower_index > 0:
+    if array[lower_index - 1] == target:
+      lower_index -= 1
+      continue
+    break
+    
+  upper_index = initial_index
+  # upper search
+  while upper_index < len(array) - 1:
+    if array[upper_index + 1] == target:
+      upper_index += 1
+      continue
+    break
+    
+  return [lower_index, upper_index]

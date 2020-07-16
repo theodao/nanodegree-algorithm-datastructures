940f37fbf872bff156282493ce031f14d6a02a79
Theo Dao <trungdao912@gmail.com>
2020-07-16 15:32:25 +0700

    recursive binary searach

---
+++ Chapter3/binary_search/binary_search.py
@@ -0,0 +1,37 @@
+def binary_search(array, target):
+  start = 0
+  end = len(array) - 1
+
+  while start <= end:
+    mid = (start + end) // 2
+
+    if array[mid] == target:
+      return mid
+    
+    if array[mid] < target:
+      start = mid + 1
+    
+    if array[mid] > target:
+      end = mid - 1
+  
+  return -1
+
+def recursion_binary_search(array, target, current_index = 0):
+  if len(array) == 0:
+    return
+
+  mid = (len(array) - 1) // 2
+
+  if array[mid] == target:
+    return mid + current_index
+  
+  if array[mid] > target:
+    return recursion_binary_search(array[:mid], target, current_index + mid)
+  
+  if array[mid] < target:
+    return recursion_binary_search(array[mid + 1:], target, current_index)
+
+print(recursion_binary_search([1,2,3,4,5,6], 2))
+
+def find_first_and_last_index(array, target):
+  return target

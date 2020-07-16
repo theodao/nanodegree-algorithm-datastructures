b80a1b604c52523e3094cb37be2f18798a77d3af
Theo Dao <trungdao912@gmail.com>
2020-07-17 00:38:08 +0700

    fix active_directory false case

--- Chapter2/data_structures/active_directory.py
+++ Chapter2/data_structures/active_directory.py
@@ -19,7 +19,10 @@ class Group(object):
   def get_name(self):
     return self.name
 
+
 def is_user_in_group(user, group):
+  global is_user
+  is_user = False
   """
   Return True if user is in the group, False otherwise.
 
@@ -27,25 +30,36 @@ def is_user_in_group(user, group):
     user(str): user name/id
     group(class:Group): group to check user membership against
   """
-  if user in group.get_users():
-    return True
-  else:
-    if len(group.get_groups()) == 0:
-      return False
 
-    for group in group.get_groups():
-      return is_user_in_group(user, group)
+  def helper(user, group):
+    if user in group.get_users():
+      global is_user
+      is_user = True
+    else:
+      if len(group.get_groups()) == 0:
+        return
 
+      for group in group.get_groups():
+        helper(user, group)
+  
+  helper(user, group)
 
-  return None
+  return is_user
+
 
 
 parent = Group("parent")
 child = Group("child")
 sub_child = Group("subchild")
+sub_child_2 = Group("subchild2")
 
 sub_child_user = "sub_child_user"
-sub_child.add_user(sub_child_user)
+sub_child_2.add_user(sub_child_user)
 
 child.add_group(sub_child)
+child.add_group(sub_child_2)
 parent.add_group(child)
+
+print(is_user_in_group(sub_child_user, parent))
+
+# print(is_user)
